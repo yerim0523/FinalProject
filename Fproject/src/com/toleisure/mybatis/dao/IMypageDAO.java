e38c36c66fa45d0efb43ec4c87b1a2b1eec6cc0b
seongjo <lee0528kr@naver.com>
2019-12-26 19:41:28 +0900

    마이페이지 분리

--- Fproject/src/com/toleisure/mybatis/dao/IMypageDAO.java
+++ Fproject/src/com/toleisure/mybatis/dao/IMypageDAO.java
@@ -5,7 +5,13 @@
 
 package com.toleisure.mybatis.dao;
 
-public class IMypageDAO
+import com.toleisure.mybatis.dto.MemberDTO;
+
+public interface IMypageDAO
 {
- 
+	public MemberDTO myInfo(String memId);		// 세션아이디를 통해 myPage 에 정보를 띄워주기 위한 메소드
+	public int infoUpdate(MemberDTO m);			// 개인정보 수정시 수행되는 메소드
+	public int updatePw(MemberDTO dto);			// 패스워드 변경 메소드
+	public String pwCheck(MemberDTO dto);		// 패스워드 확인 메소드
+	public int changeMyPw(MemberDTO dto);		// 마이페이지 패스워드 변경 메소드
 }
