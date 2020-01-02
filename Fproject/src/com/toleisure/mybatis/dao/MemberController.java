aceceeb41046cd9f7e2afeb8fae3ff4b271ab8e1
yerim <qkfkaroql96@naver.com>
2020-01-02 17:16:13 +0900

    푸시합니다

--- Fproject/src/com/toleisure/mybatis/dao/MemberController.java
+++ Fproject/src/com/toleisure/mybatis/dao/MemberController.java
@@ -1,21 +1,25 @@
 package com.toleisure.mybatis.dao;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Iterator;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.fileupload.FileItem;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -201,14 +205,14 @@ public class MemberController
       return view;
    }
    
-   @RequestMapping(value = "/memberinsert.action", method = RequestMethod.POST)
+   @RequestMapping(value = "/memberinsert.action", method = {RequestMethod.POST,RequestMethod.GET})
    public String memberinsert(MemberDTO m, MultipartHttpServletRequest request)
    {
       String view = "WEB-INF/views/joinTest.jsp";
       
-      String rootUploadDir = "C:"+File.separator+"Upload"; // C:/Upload
+      String rootUploadDir = "C:"+File.separator+"uploads"; // C:/Upload
       
-      File dir = new File(rootUploadDir + File.separator + "testfile"); 
+      File dir = new File(rootUploadDir); 
       
       if(!dir.exists()) { //업로드 디렉토리가 존재하지 않으면 생성
           dir.mkdirs();
@@ -269,8 +273,8 @@ public class MemberController
    }
    
    @RequestMapping(value="/fileuploadtest.action", method = RequestMethod.POST)
-   public String fileUploadTest(MultipartHttpServletRequest request, Model model) {
-       
+   public String fileUploadTest(MultipartHttpServletRequest request, Model model)throws ServletException, IOException
+   {
        String rootUploadDir = "C:"+File.separator+"Upload"; // C:/Upload
        
        File dir = new File(rootUploadDir + File.separator + "testfile"); 
@@ -285,7 +289,6 @@ public class MemberController
        String uploadFileName;
        MultipartFile mFile = null;
        String orgFileName = ""; //진짜 파일명
-       String sysFileName = ""; //변환된 파일명
        
        ArrayList<String> list = new ArrayList<String>();
        
@@ -300,16 +303,11 @@ public class MemberController
            
            if(orgFileName != null && orgFileName.length() != 0) { //sysFileName 생성
                System.out.println("if문 진입");
-               SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMDDHHmmss-" + fileLoop);
-               Calendar calendar = Calendar.getInstance();
-               sysFileName = simpleDateFormat.format(calendar.getTime()); //sysFileName: 날짜-fileLoop번호
-               
                
                try {
                    System.out.println("try 진입");
-                   mFile.transferTo(new File(dir + File.separator + sysFileName)); // C:/Upload/testfile/sysFileName
-                   list.add("원본파일명: " + orgFileName + ", 시스템파일명: " + sysFileName);
-                   
+                   mFile.transferTo(new File(dir + File.separator + orgFileName)); // C:/Upload/testfile/sysFileName
+                   list.add("원본파일명: " + orgFileName);
                }catch(Exception e){
                    list.add("파일 업로드 중 에러발생!!!");
                }
